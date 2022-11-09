937dded01c686958af2be8e89f105f31f5429034
venkat-ram13 <115686426+venkat-ram13@users.noreply.github.com>
2022-11-09 01:55:58 -0800

    Update IIFE.js

--- IIFE.js
+++ IIFE.js
@@ -39,3 +39,50 @@ console.log(total)
 ([3,22,5,62,71]);
 
 
+
+//median
+
+var arr1 = [2,5,21,11];
+var arr2 = [9,3,12,17];
+(function (arr1,arr2){
+var arr3 = arr1.concat(arr2);
+console.log(arr3);
+var sort = arr3.sort(function (a,b){return a-b});
+console.log(sort);
+if(sort.length%2!=0){
+    var median = sort[Math.floor(sort.length/2)];
+    console.log(median);
+}else{
+    var median = Math.floor((sort[sort.length/2] + sort[sort.length/2-1])/2);
+    console.log(median)
+   }
+});
+(arr1,arr2);
+
+
+//Return all the palindromes in an array
+
+var palin = ["abcba","java","11211","php"]
+
+(function(str){
+    var arr = str.filter((val)=>{
+        return val.split("").reverse().join("")===val;
+    })
+    console.log(arr)
+})
+(palin);
+
+
+
+//Convert all the strings to title caps in a string array
+	
+(function(str){
+    for(let i=0;i<str.length;i++)
+    {
+        str[i] = str[i][0].toUpperCase()+str[i].slice(1).toLowerCase();
+    }
+    console.log(str.join(" "));
+})
+(["this","is","the","reality","accept","it"]);
+
+
